d693a8d35591477afdb3dceb4fd609e8099bc264
Chris Carty <chriscartydev@gmail.com>
2020-05-18 21:15:15 -0400

    removed projects

--- rancher-k8s-import/gke/rancher.tf
+++ rancher-k8s-import/gke/rancher.tf
@@ -10,22 +10,22 @@ resource "rancher2_cluster" "gke-import" {
   name        = "gke-master"
   description = "Created and Added through GitOps!"
 
-  enable_cluster_monitoring =  true
+  enable_cluster_monitoring =  false
 }
 
-resource "rancher2_project" "flux" {
-  provider    = rancher2.rancher-test
-  name = "flux-system"
-  cluster_id = rancher2_cluster.gke-import.cluster_registration_token[0].cluster_id
+# resource "rancher2_project" "flux" {
+#   provider    = rancher2.rancher-test
+#   name = "flux-system"
+#   cluster_id = rancher2_cluster.gke-import.cluster_registration_token[0].cluster_id
 
-}
+# }
 
-resource "rancher2_project" "demo" {
-  provider    = rancher2.rancher-test
-  name = "demo-app"
-  cluster_id = rancher2_cluster.gke-import.cluster_registration_token[0].cluster_id
+# resource "rancher2_project" "demo" {
+#   provider    = rancher2.rancher-test
+#   name = "demo-app"
+#   cluster_id = rancher2_cluster.gke-import.cluster_registration_token[0].cluster_id
 
-}
+# }
 
 output "cluster" {
   value = rancher2_cluster.gke-import.cluster_registration_token
